3212e7630fc2123c96d5f58b837c825c4c15b026
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-21 22:21:28 +0300

    [BF-70] first commit. Init empty project.

---
+++ src/main/kotlin/ru/fdo/bank/finrez/clientportfolioview/ClientPortfolioViewApplication.kt
@@ -0,0 +1,11 @@
+package ru.fdo.bank.finrez.clientportfolioview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class ClientPortfolioViewApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClientPortfolioViewApplication>(*args)
+}
---
+++ src/test/kotlin/ru/fdo/bank/finrez/clientportfolioview/ClientPortfolioViewApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.fdo.bank.finrez.clientportfolioview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClientPortfolioViewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
